7754e13ff123443df1248315695429808a5a9a07
JJ Buckley <jjbuckley@gmail.com>
2014-03-26 00:15:45 +0100

    Adds i18n, which will be hooked up to a backend
    The I18n backend will be hooked up to a database. Then the translations can be
    loaded from there (cached, of course, locally based on timestamps), and the
    frontend can have a client which can edit the copy _on the page_, and when it
    gets saved, it'll be sent to the database (updating the changed translations,
    and invalidating the cache).

--- app.rb
+++ app.rb
@@ -1,8 +1,48 @@
 require 'sinatra'
+require 'i18n'
+require 'yaml'
+
+I18n.enforce_available_locales = true
+I18n.load_path = Dir["locale/*.yml"]
+
+pages = Dir[File.dirname(__FILE__) + "/views/*.haml"].map do |f|
+  File.basename(f, ".haml")
+end - %w(layout)
+
+set :pages, pages
+
+configure do
+  # Called once
+end
+
+before do
+  # Called before each request
+end
+
+helpers do
+  def t(*args)
+    I18n.translate(*args)
+  end
+
+  def link_to(text, attrs)
+    attrs = attrs.map { |k, v| "#{k}='#{v}'" }.join(' ')
+    "<a %s>%s</a>" % [ attrs, text ]
+  end
+end
 
 get '/' do
   haml :index
 end
 
-get '/:page.html' do
+get "/debug" do
+  content_type :txt
+  {
+    i18n: I18n.available_locales
+  }.to_yaml
+end
+
+pages.each do |page|
+  get "/#{page}" do
+    haml :"#{page}"
+  end
 end
