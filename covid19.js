42e8050bececedea4b52d449df66d2b473524c58
JJ Buckley <jj@bjjb.org>
2020-06-10 09:59:04 +0200

    Removes the Covid-19 notice and updates packages
    - deletes the stylesheet and JS file (they can be retrieved from history
      if needed later)
    - removes the `<aside>` with the notice text
    - also updates a few npm packages

--- covid19.js
+++
@@ -1,6 +0,0 @@
-addEventListener('load', function() {
-  const notice = document.querySelector('aside.notice');
-  notice.querySelector('a.close').addEventListener('click', function() {
-    notice.remove()
-  })
-});
